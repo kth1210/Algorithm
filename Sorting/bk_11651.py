89332b8ff6336facc934b4f81016e65a9bfe57b6
kth1210 <k2hop1210@gmail.com>
2022-12-02 20:34:55 +0900

    12/2

---
+++ Sorting/bk_11651.py
@@ -0,0 +1,13 @@
+import sys
+
+N = int(sys.stdin.readline().rstrip())
+arr = []
+
+for _ in range(N):
+    x, y = map(int, sys.stdin.readline().split())
+    arr.append((x,y))
+
+arr.sort(key=lambda x: (x[1], x[0]))
+
+for idx in range(N):
+    print(arr[idx][0], arr[idx][1])
