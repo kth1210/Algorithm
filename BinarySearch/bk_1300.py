37b50e8fdcbcfce52ce2a3fa168685c65f588235
kth1210 <k2hop1210@gmail.com>
2023-01-02 23:46:38 +0900

    1/2

---
+++ BinarySearch/bk_1300.py
@@ -0,0 +1,25 @@
+import sys
+
+N = int(sys.stdin.readline().rstrip())
+K = int(sys.stdin.readline().rstrip())
+
+start = 1
+end = N*N
+# end = K
+result = 0
+
+while start <= end:
+    cnt = 0
+
+    mid = (start + end) // 2
+
+    for div in range(1, N + 1):
+        cnt += min(mid // div, N)
+    
+    if cnt >= K:
+        result = mid
+        end = mid - 1
+    else:
+        start = mid + 1
+
+print(result)
