269a6400ff237af7390e7838e5ba1df65ceb0dda
kth1210 <k2hop1210@gmail.com>
2023-01-06 01:04:49 +0900

    1/6

---
+++ ETC/bk_2442.py
@@ -0,0 +1,13 @@
+import sys
+
+N = int(sys.stdin.readline().rstrip())
+
+for i in range(N):
+    for j in range(i+1, N):
+        print(" ", end="")
+    for k in range(i):
+        print("*", end="")
+    print("*", end="")
+    for l in range(i):
+        print("*", end="")
+    print()
