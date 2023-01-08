00530d5f61840fa0da4f5106ca1f25b3215c9695
kth1210 <k2hop1210@gmail.com>
2023-01-09 01:04:00 +0900

    1/8 3020_solved

--- BinarySearch/bk_3020.py
+++ BinarySearch/bk_3020.py
@@ -1,5 +1,21 @@
 import sys
 
+def bin_search(arr, val, start, end):
+    result = len(arr)
+    while (start <= end):
+        mid = (start + end) // 2
+
+        if arr[mid] < val:
+            # result = mid
+            start = mid + 1
+        # elif arr[mid] == val:
+        #     result = mid
+        #     start
+        else:
+            result = mid
+            end = mid - 1
+    return result
+
 N, H = map(int, sys.stdin.readline().split())
 
 bottom = []
@@ -11,39 +27,19 @@ for idx in range(N):
     else:
         top.append(int(sys.stdin.readline().rstrip()))
 
-# print(odd)
-# print(even)
-
 bottom.sort()
 top.sort()
-# print(bottom)
-# print(top)
 
 result_val = float("inf")
 result_cnt = 0
 
-def bin_search(arr, val, start, end):
-    result = val
-    while (start <= end):
-        mid = (start + end) // 2
-
-        if arr[mid] < val:
-            # end = mid - 1
-            result = mid
-            start = mid + 1
-        elif arr[mid] == val:
-            return mid + 1
-        else:
-            # result = mid
-            # start = mid + 1
-            end = mid - 1
-    return result
-
-# print()
 for idx in range(1, H+1):
     val = bin_search(bottom, idx, 0, N//2 - 1) + bin_search(top, H-idx+1, 0, N//2 - 1)
-    # print(val)
+    # print(bin_search(bottom, idx, 0, N//2 - 1))
+    # print(bin_search(top, H-idx+1, 0, N//2 - 1))
+    # print()
     
+    val = N - val
     if result_val > val:
         result_val = val
         result_cnt = 1
@@ -51,7 +47,3 @@ for idx in range(1, H+1):
         result_cnt += 1
 
 print(result_val, result_cnt)
-
-
-
-    
